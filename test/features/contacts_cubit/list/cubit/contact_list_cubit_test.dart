ad203b09a767eb84812d647298d4a728b9b2dc12
Ita Chesterton win <itajacy@gmail.com>
2024-07-08 14:09:17 -0300

    testes com cubit

--- test/features/contacts_cubit/list/cubit/contact_list_cubit_test.dart
+++ test/features/contacts_cubit/list/cubit/contact_list_cubit_test.dart
@@ -1,27 +1,46 @@
-import 'package:contact_bloc/features/contacts/list/bloc/contact_list_bloc.dart';
+import 'package:bloc/bloc.dart';
+import 'package:bloc_test/bloc_test.dart';
 import 'package:contact_bloc/features/contacts_cubit/list/cubit/contact_list_cubit.dart';
 import 'package:contact_bloc/models/contact_model.dart';
 import 'package:contact_bloc/repositories/contacts_repository.dart';
 import 'package:flutter_test/flutter_test.dart';
+import 'package:mocktail/mocktail.dart';
 
-import '../../../contacts/list/bloc/contact_list_bloc_test.dart';
+class MockContactsRepository extends Mock implements ContactsRepository {}
 
 void main() {
   //! declaração
 
   late ContactsRepository repository;
-  late ContactListCubit bloc;
+  late ContactListCubit cubit;
   late List<ContactModel> contacts;
 
   //! preparação
 
   setUp(() {
     repository = MockContactsRepository();
-    bloc = ContactListCubit(repository: repository);
+    cubit = ContactListCubit(repository: repository);
     contacts = [
       ContactModel(
           name: 'Rodrigo Rahman', email: 'rodrigo@academiadoflutter.com.br'),
       ContactModel(name: 'Rodrigo', email: 'rodrigo@gmail.com.br'),
     ];
   });
+
+//! execução
+
+  blocTest<ContactListCubit, ContactListCubitState>(
+    'Deve buscar os contatos',
+    build: () => cubit,
+    act: (cubit) => cubit.findAll(),
+    setUp: () {
+      when(
+        () => repository.findAll(),
+      ).thenAnswer((_) async => contacts);
+    },
+    expect: () => [
+      ContactListCubitState.loading(),
+      ContactListCubitState.data(contacts: contacts),
+    ],
+  );
 }
